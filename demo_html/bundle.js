4756ae832c2feae9fe691b1bccf233ebbe591062
Gerardo Lima <gerardo.lima@gmail.com>
2016-11-03 10:33:47 +0000

    fix demo_html

--- demo_html/bundle.js
+++ demo_html/bundle.js
@@ -1,22 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var numFormmater = require('gerardolima-ts-app');
-var formattedNum = numFormmater(234324234);
+var myAppInstance = require('gerardolima-ts-app');
+var msg = "application instance name: " + myAppInstance.get_name() + "\n"
+        + "double(1): " + myAppInstance.constructor.double(1); 
 
 document.getElementById('date-placeholder').innerText = (new Date()).toString();
-document.getElementById('output-placeholder').innerText = formattedNum; 
+document.getElementById('output-placeholder').innerText = msg; 
 
-
-},{"gerardolima-ts-app":2}],2:[function(require,module,exports){
+},{"gerardolima-ts-app":3}],2:[function(require,module,exports){
 "use strict";
-var myLocaleString = require("./myLocaleString");
-module.exports = myLocaleString;
+var MyAppClass = (function () {
+    // constructor
+    function MyAppClass(name) {
+        this.name = name;
+    }
+    MyAppClass.prototype.get_name = function () { return this.name; };
+    MyAppClass.double = function (x) { return 2 * x; };
+    return MyAppClass;
+}());
+exports.MyAppClass = MyAppClass;
 
-},{"./myLocaleString":3}],3:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 "use strict";
-function myLocaleString(n, locale) {
-    return n.toLocaleString(locale);
-}
-;
-module.exports = myLocaleString;
+var MyAppClass_1 = require("./MyAppClass");
+module.exports = new MyAppClass_1.MyAppClass("this-is-an-app-instance");
 
-},{}]},{},[1]);
+},{"./MyAppClass":2}]},{},[1]);
--- demo_html/index.js
+++ demo_html/index.js
@@ -1,6 +1,6 @@
-var numFormmater = require('gerardolima-ts-app');
-var formattedNum = numFormmater(234324234);
+var myAppInstance = require('gerardolima-ts-app');
+var msg = "application instance name: " + myAppInstance.get_name() + "\n"
+        + "double(1): " + myAppInstance.constructor.double(1); 
 
 document.getElementById('date-placeholder').innerText = (new Date()).toString();
-document.getElementById('output-placeholder').innerText = formattedNum; 
-
+document.getElementById('output-placeholder').innerText = msg; 
